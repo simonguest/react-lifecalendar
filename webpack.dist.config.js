57b8926bf432db99390443a7f77bc0620f03a934
Simon Guest <me@simonguest.com>
2016-10-31 14:37:48 -0700

    More polished webpack

---
+++ webpack.dist.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: '#cheap-module-eval-source-map',
+  entry: [
+    './src/index'
+  ],
+  resolveLoader: {
+    root: path.join(__dirname, 'node_modules')
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        exclude: /node_modules/,
+        include: __dirname
+      }
+    ]
+  }
+};
